85c0d4f74f8cf1c0b33e44896cfd7979b6f4b3ad
mc5312 <124803717+mc5312@users.noreply.github.com>
2023-12-21 23:36:08 +0800

    update

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ import util
 import async_call
 import sync_call
 import json
-from pydantic import BaseModel
-from typing import Optional
 import requests
 import pandas as pd
-from pandas import json_normalize
-import datetime
 
 app = FastAPI()
 util.init()
@@ -21,12 +17,57 @@ subscription_fields = [
 ]
 
 
+# Internal Functions
+
 def get_full_subscription():
     res = requests.get(util.resources['subscription'] + 'full')
     subs_df = pd.concat([pd.DataFrame(item[0]) for item in res.json()], axis=1).transpose()
     subs_df.columns = subscription_fields
     return subs_df
 
+
+def create_subscription_custom(user_id, report_id) -> dict:
+    return_dict = {}
+
+    res = requests.post(util.resources['subscription'] + str(user_id) + '/' + str(report_id))
+
+    return_dict['subscription_id'] = res.json()['Created subscription']
+
+    print(" *** Subscription Created: subscription_id=" + str(return_dict['subscription_id']))
+    print(" ****** User " + str(user_id) + ' is now subscribing to report ' + str(report_id))
+    print()
+    return return_dict
+
+
+def show_stat_custom(sync_type: str):
+    result = None
+
+    # construct call list
+    resource_paths = [
+        {"resource": "MS-3-Subscription", "url": util.resources['subscription'] + "full"},
+        {"resource": "MS-1-User", "url": util.resources['user']},
+        {"resource": "MS-2-Report", "url": util.resources['report']}
+    ]
+
+    s_time = time.time()
+    print("=== Request Order:", [res["resource"] for res in resource_paths], "===")
+    util.response_order = []
+    if sync_type == 'sync':
+        print(' *** Running Sync Calls')
+        result = sync_call.sync_request(resource_paths)
+    else:
+        print(' *** Running Async Calls')
+        result = async_call.async_request(resource_paths)
+    return_dict = {k: len(v) for (k, v) in result.items()}
+    print("Result: ", return_dict)
+    print("=== Response Order:", util.response_order, "===")
+    print("Time used: ", time.time() - s_time)
+    print()
+
+    return return_dict
+
+
+# Fast API Functions
 
 @app.get("/")
 async def root():
@@ -35,69 +76,14 @@ async def root():
     }
 
 
-# noinspection DuplicatedCode
-@app.post("/create_report")
-async def create_subscription(data=Body(...), response=Response):
-    return_dict = {}
-
-    # POST Subscription
-    subs_df = get_full_subscription()
-    next_id = subs_df['subscription_id'].max() + 1
-    post_subs_dict = {
-        "subscription_id": next_id,
-        "user_id": int(data['user_id']),
-        "report_id": int(data['user_id']),
-        "analyst_id": '',
-        "subscription_date": datetime.datetime.now().strftime("%b %d, %Y"),
-        "feedbacks": "",
-        "notifications": "",
-        "activity": ""
-    }
-    res = requests.post(util.resources['subscription'], json=post_subs_dict)
-    return_dict['subscription_id'] = res.json()['Created']['subscription_id']
-    return_dict['user_id'] = res.json()['Created']['user_id']
-    print(" *** Subscription Created: subscription_id=" + str(res.json()['Created']['subscription_id']))
-    print(
-        " ****** User " + str(res.json()['Created']['user_id']) + ' is now subscribing to report ' +
-        str(res.json()['Created']['report_id'])
-    )
-    print()
-
-    return return_dict
-
-
-# noinspection DuplicatedCode
 @app.post("/create_subscription")
 async def create_subscription(data=Body(...), response=Response):
     return_dict = {}
-
-    # POST Subscription
-    subs_df = get_full_subscription()
-    next_id = subs_df['subscription_id'].max() + 1
-    post_subs_dict = {
-        "subscription_id": next_id,
-        "user_id": int(data['user_id']),
-        "report_id": int(data['user_id']),
-        "analyst_id": '',
-        "subscription_date": datetime.datetime.now().strftime("%b %d, %Y"),
-        "feedbacks": "",
-        "notifications": "",
-        "activity": ""
-    }
-    res = requests.post(util.resources['subscription'], json=post_subs_dict)
-    return_dict['subscription_id'] = res.json()['Created']['subscription_id']
-    return_dict['user_id'] = res.json()['Created']['user_id']
-    print(" *** Subscription Created: subscription_id=" + str(res.json()['Created']['subscription_id']))
-    print(
-        " ****** User " + str(res.json()['Created']['user_id']) + ' is now subscribing to report ' +
-        str(res.json()['Created']['report_id'])
-    )
-    print()
-
+    return_dict.update(create_subscription_custom(data['user_id'], data['report_id']))
     return return_dict
 
 
-@app.post("/delete_subscription")
+@app.delete("/delete_subscription")
 async def delete_subscription(data=Body(...), response=Response):
     return_dict = {}
 
@@ -108,7 +94,7 @@ async def delete_subscription(data=Body(...), response=Response):
         & (subs_df['report_id'] == int(data['report_id']))
         ]
     return_dict['subscription_id'] = subs_df['subscription_id'].values
-    print(" *** Found subscription: " + ', '.join(return_dict['subscription_id']))
+    print(" *** Found subscription: " + str(return_dict['subscription_id']))
     if len(subs_df) > 0:
         for subscription_id in return_dict['subscription_id']:
             res = requests.delete(util.resources['subscription'] + str(subscription_id))
@@ -117,19 +103,11 @@ async def delete_subscription(data=Body(...), response=Response):
         print(" ****** No subscription found on (" + str(data['user_id']) + ", " + str(data['report_id']) + ")")
     print()
 
-    return return_dict
+    return list(return_dict['subscription_id'])
 
 
 @app.post("/create_report")
 async def create_report(data=Body(...), response=Response):
-    # Input:
-    # {
-    #     "title": "POST Report TEST",
-    #     "analyst_id": "5",
-    #     "content": "this is the new report I added for the video",
-    #     "feedback": "very good video report"
-    # }
-
     return_dict = {}
 
     # POST Report
@@ -143,27 +121,7 @@ async def create_report(data=Body(...), response=Response):
     print(" ****** Report title '" + res.json()['title'] + "' by Analyst " + str(res.json()['analyst_id']))
     print()
 
-    # POST Subscription
-    subs_df = get_full_subscription()
-    next_id = subs_df['subscription_id'].max() + 1
-    post_subs_dict = {
-        "subscription_id": next_id,
-        "user_id": int(data['analyst_id']),
-        "report_id": created_report_id,
-        "analyst_id": int(data['analyst_id']),
-        "subscription_date": datetime.datetime.now().strftime("%b %d, %Y"),
-        "feedbacks": "test_feedbacks",
-        "notifications": "test_notification2",
-        "activity": "test_activity"
-    }
-    res = requests.post(util.resources['subscription'], json=post_subs_dict)
-    return_dict['subscription_id'] = res.json()['Created']['subscription_id']
-    return_dict['user_id'] = res.json()['Created']['user_id']
-    print(" *** Subscription Created: subscription_id=" + str(res.json()['Created']['subscription_id']))
-    print(
-        " ****** User " + str(res.json()['Created']['user_id']) + ' is now subscribing to report ' +
-        str(res.json()['Created']['report_id'])
-    )
+    return_dict.update(create_subscription_custom(data['analyst_id'], created_report_id))
     print()
 
     return return_dict
@@ -196,27 +154,27 @@ async def delete_report(report_id: int, response: Response):
     return return_dict
 
 
-@app.get("/user_subscription/{user_id}")
+@app.get("/get_report_by_user/{user_id}")
 async def get_report_by_user(user_id: int, response: Response):
     return_dict = {}
 
     # GET List of report_id FROM /subscription BY user_id
-    res = requests.get(util.resources['subscription'] + 'user_id/' + str(user_id))
+    res = requests.get(util.resources['subscription'] + 'user/' + str(user_id))
 
     if res.status_code != 200:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return res.json()
-    return_dict['report_id'] = res.json()['report_id']
+    return_dict['report_id'] = [item[0] for item in res.json()[1]]
     print(" *** Reports Subscribed: report_id=" + str(return_dict['report_id']))
-    print()
 
     # GET List of report titles by List of report_id
     report_titles = []
     for report_id in return_dict['report_id']:
         res = requests.get(util.resources['report'] + str(report_id))
-        report_titles += res.json()['title']
+        report_titles += [res.json()['title']]
         print(' ***** Report ' + str(report_id) + ' : ' + res.json()['title'])
     return_dict['Report Titles'] = report_titles
+    return_dict = dict(zip(return_dict['report_id'], return_dict['Report Titles']))
     print()
     return return_dict
 
@@ -229,34 +187,6 @@ async def show_stat(response: Response):
 @app.get("/show_stat/{sync_type}")
 async def show_stat(sync_type: str, response: Response):
     return show_stat_custom(sync_type)
-
-
-def show_stat_custom(sync_type: str):
-    result = None
-
-    # construct call list
-    resource_paths = [
-        # {"resource": "MS-3-Subscription", "url": util.resources['subscription'] + "full"},
-        {"resource": "MS-1-User", "url": util.resources['user']},
-        {"resource": "MS-2-Report", "url": util.resources['report']}
-    ]
-
-    s_time = time.time()
-    print("=== Request Order:", [res["resource"] for res in resource_paths], "===")
-    util.response_order = []
-    if sync_type == 'sync':
-        print(' *** Running Sync Calls')
-        result = sync_call.sync_request(resource_paths)
-    else:
-        print(' *** Running Async Calls')
-        result = async_call.async_request(resource_paths)
-    return_dict = {k: len(v) for (k, v) in result.items()}
-    print("Result: ", return_dict)
-    print("=== Response Order:", util.response_order, "===")
-    print("Time used: ", time.time() - s_time)
-    print()
-
-    return return_dict
 
 
 if __name__ == "__main__":
